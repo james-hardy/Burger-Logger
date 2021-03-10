706e707adcf2a05410243be77d54937202ee1020
james-hardy <topsec115@gmail.com>
2021-03-10 11:06:27 -0500

    styling

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,49 +1,49 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    if (event) {
-        console.info('DOM loaded');
-    };
+    // if (event) {
+    //     console.info('DOM loaded');
+    // };
 
    
-    const changeDevourBtns = document.querySelectorAll('.change-devoured');
-    // console.log(changeDevourBtns);
+    // const changeDevourBtns = document.querySelectorAll('.change-devoured');
+    // // console.log(changeDevourBtns);
 
-    if (changeDevourBtns) {
-        //build this as a submit event, make a form. remove the if. give form an id
-        //add event listener of submit on button id ate - run a fucntion 
-        //add from in htlm and give form an id
-        //get element by id and call the id of the form
-        //add event listener
-        //submit
-        //second argunemnt is function with line 28 and on. google on submit event
-        changeDevourBtns.forEach((button) => {
-            button.addEventListener('click', (e) => {
-                const id = e.target.getAttribute('data-id')
-                console.log(id);
-                const newDevour = e.target.getAttribute('.data-newdevour');
+    // if (changeDevourBtns) {
+    //     //build this as a submit event, make a form. remove the if. give form an id
+    //     //add event listener of submit on button id ate - run a fucntion 
+    //     //add from in htlm and give form an id
+    //     //get element by id and call the id of the form
+    //     //add event listener
+    //     //submit
+    //     //second argunemnt is function with line 28 and on. google on submit event
+    //     changeDevourBtns.forEach((button) => {
+    //         button.addEventListener('click', (e) => {
+    //             const id = e.target.getAttribute('data-id')
+    //             console.log(id);
+    //             const newDevour = e.target.getAttribute('.data-newdevour');
 
-                const newDevourState = {
-                    devoured: newDevour,
-                };
-                //this is the function to run anytime a button is clicked 
-                fetch(`/api/burgers/${id}`, {
-                    method: 'PUT',
-                    headers: {
-                      Accept: 'application/json',
-                      'Content-Type': 'application/json',
-                    },
+    //             const newDevourState = {
+    //                 devoured: newDevour,
+    //             };
+    //             //this is the function to run anytime a button is clicked 
+    //             fetch(`/api/burgers/${id}`, {
+    //                 method: 'PUT',
+    //                 headers: {
+    //                   Accept: 'application/json',
+    //                   'Content-Type': 'application/json',
+    //                 },
 
-                    body: JSON.stringify(newDevourState),
-                }).then((response) => {
-                    if (response.ok) {
-                        console.log(`changed devour to: ${newDevour}`);
-                        location.reload('/');
-                    } else {
-                        alert('soemthing went wrong');
-                    }
-                });
-            });
-        });
-    }
+    //                 body: JSON.stringify(newDevourState),
+    //             }).then((response) => {
+    //                 if (response.ok) {
+    //                     console.log(`changed devour to: ${newDevour}`);
+    //                     location.reload('/');
+    //                 } else {
+    //                     alert('soemthing went wrong');
+    //                 }
+    //             });
+    //         });
+    //     });
+    // }
     //commiting to heroku
     //post burger to devour
     const createBurgerBtn = document.getElementById('create-form');
